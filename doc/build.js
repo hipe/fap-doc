0d932591293d62da9e9d08c509314173ada9d7da
Mark Meves <mark.meves@gmail.com>
2011-03-04 18:37:35 -0500

    singularize the folder names in build.js

--- doc/build.js
+++ doc/build.js
@@ -1,8 +1,10 @@
 #!/usr/bin/env node
 ;
 
+path = require('path');
+
 require.paths.push(__dirname);
-var petroot = __dirname + '/../vendor/petrify-hipe';
+var petroot = path.normalize(__dirname + '/../vendor/petrify-hipe');
 require.paths.push(petroot + '/lib');
 require.paths.push(petroot + '/deps');
 require.paths.push(petroot + '/deps/json-template/lib');
@@ -12,8 +14,8 @@ var buildrunner = require('buildrunner');
 
 buildrunner.run({
     data_dir:     __dirname + '/data',
-    view_dir:     __dirname + '/views',
-    template_dir: __dirname + '/templates',
+    view_dir:     __dirname + '/view',
+    template_dir: __dirname + '/template',
     output_dir:   __dirname + '/www',
     media_dirs:  [__dirname + '/media']
 });
