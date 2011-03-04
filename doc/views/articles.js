3b4220acf104ce81d9fe4d032ecebca82f557765
Mark Meves <mark.meves@gmail.com>
2011-03-04 17:08:09 -0500

    modify doc/views/articles.js so that it uses ctime for the date property
      @todo this is probably not right

--- doc/views/articles.js
+++ doc/views/articles.js
@@ -1,3 +1,9 @@
+var fs = require('fs'),
+  path = require('path');
+
+// @todo would be better not hardcoded etc.
+var datadir = path.normalize(__dirname + '/../data/');
+
 // wait until the navigation partial is complete
 exports.requires = ['navigation'];
 
@@ -7,8 +13,15 @@ exports.run = function(view, context){
     // parse meta data - this updates the context.data in-place, so
     // other views can make use of these changes
     context.data = context.data.map(function(x){
-        x.tags = x.metadata.tags.split(', ');
-        x.date = new Date(x.metadata.date);
+        if (x.metadata.tags) {
+          x.tags = x.metadata.tags.split(', ');
+        }
+        if (x.metadata.date) {
+          x.date = new Date(x.metadata.date);
+        } else {
+          var stat = fs.statSync(datadir + x.filename);
+          if (stat && stat.ctime) x.date = new Date(stat.ctime);
+        }
         x.url = x.filename.replace(/\.md$/, '.html');
         return x;
     });
