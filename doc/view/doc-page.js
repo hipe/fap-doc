0e90bd3b458dbd0e87fcf1829b265286e7495261
Mark Meves <mark.meves@gmail.com>
2011-03-07 01:49:02 -0500

    doc: we can't pre-render navigation partial just once:
      it looks different on each page!

--- doc/view/doc-page.js
+++ doc/view/doc-page.js
@@ -1,10 +1,21 @@
 
-exports.requires = ['navigation'];
+exports.requires = ['initial'];
 
 exports.run = function(view, context){
 
   // for each doc-page emit the doc-page template
-  context.data.forEach(function(page){
+  context.data.forEach(function(page, idx){
+    var url = page.url;
+    context.data.forEach(function(page2){
+      page2.selected = (url == page2.url);
+    });
+
+    context.partials.navigation =
+      context.templates['nav.jsont'].expand({
+        sortedDocPages : context.data,
+        pageIndex : idx
+    });
+
     var html = context.templates['doc-page.jsont'].expand({
       docPage: page,
       partials: context.partials
