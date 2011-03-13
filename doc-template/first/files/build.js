8295aa311cbadbc83582679b1e8f57744e8809d7
Mark Meves <mark.meves@gmail.com>
2011-03-13 17:31:17 -0400

    Now that we learned a little bit more how to work with require.paths,   we don't need as much convoluted code (libs.js) to pull in our libraries
    NOTE: this guy successfully generates working whodily hahs

--- doc-template/first/files/build.js
+++ doc-template/first/files/build.js
@@ -1,52 +1,29 @@
 #!/usr/bin/env node
 ;
-var Path = require('path'), sys = require('sys');
 
-var dircheck = function(path, name) {
-  name || (name = 'path');
-  if (!Path.existsSync(path)) {
-    sys.puts(name + " not found: "+path);
-    sys.puts("(from "+__filename+")");
-    return false;
-  }
-  return true;
-};
+var fap = {fap-doc-root};
+require.paths.push(fap + '/lib');
+require.paths.push({petrify-root}/lib');
+require.paths.push({petrify-root}/deps');
+require.paths.push({petrify-root}/deps/json-template/lib');
+require.paths.push({petrify-root}/deps/markdown-js/lib');
 
-(function(){
 
-require.paths.push(__dirname);
-var projRoot    = Path.normalize(__dirname + '/..');
-var fapDocRoot  = {fap-doc-root};
-var petrifyRoot = {petrify-root};
-
-if (!dircheck(fapDocRoot, 'fap-doc root')) return;
-if (!dircheck(petrifyRoot, 'petrify root')) return;
-
-require.paths.push(petrifyRoot + '/lib');
-require.paths.push(petrifyRoot + '/deps');
-require.paths.push(petrifyRoot + '/deps/json-template/lib');
-require.paths.push(petrifyRoot + '/deps/markdown-js/lib');
-
-var buildrunner = require('buildrunner');
-var petrify = require('petrify');
-
-// start petrify hacks
-require(fapDocRoot + '/lib/fap-doc/petrify-hacks').
-  enableHacks(petrify, buildrunner);
-buildrunner.hackIncludeStrangeDataFiles(['../../README.md']);
-buildrunner.hackSetMetadata('../../README.md', {
-  directoryIndex : 1,
-  pageTitle : "{project-label}"
-});
-// end petrify hacks
-
-buildrunner.run({
-    data_dir:     __dirname + '/data',
-    view_dir:     __dirname + '/view',
-    template_dir: __dirname + '/template',
-    output_dir:   __dirname + '/www',
-    media_dirs:  [__dirname + '/media']
+require('fap-doc/petrify-hacks').hackPetrify(function(h) {
+  h.processCodeBlocks();
+  h.includeStrangeDataFiles(['../../README.md']);
+  h.setMetadata('../../README.md', {
+    'directory-index'   : 1,
+    'page-title'        : "{project-label}",
+    'page-title-short'  : "fuckparse",
+    'code-blocks'       : '{"0":"js", "*":"none"}'
+  });
 });
 
-
-})();
+require('buildrunner').run({
+  data_dir:     __dirname + '/data',
+  view_dir:     __dirname + '/view',
+  template_dir: __dirname + '/template',
+  output_dir:   __dirname + '/www',
+  media_dirs:  [__dirname + '/media']
+});
