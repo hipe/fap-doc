9c05e81c686de7ac1d4d5c155d012fd4b8b2602c
Mark Meves <mark.meves@gmail.com>
2011-03-24 07:15:58 -0400

    in generated doc tree, fix pageTitle so we don't swallow   the first header

--- doc-template/first/files/view/initial.js
+++ doc-template/first/files/view/initial.js
@@ -7,33 +7,37 @@ exports.run = function(view, context) {
   // other views can make use of these changes
   var maxIndex = -1;
   var datas = context.data;
-  datas.forEach(function(x){
+  datas.forEach(function(x) {
     // tags and date processing omitted (keep this line for now)
 
     // remove leading '../' per haxies
     var rootie = x.filename.replace(/^(\.\.\/)+/,'').replace(/\.md$/, '');
     x.url = rootie + '.html';
 
+    // give directory indexes for use in nav
     if (undefined == x.metadata['directory-index']) {
       x.directoryIndex = (++ maxIndex);
     } else {
       x.directoryIndex = x.metadata['directory-index'];
-      if (x.directoryIndex > maxIndex) {
-        maxIndex = x.directoryIndex;
-      }
+      if (x.directoryIndex > maxIndex) maxIndex = x.directoryIndex;
     }
 
-    x.pageTitle = x.metadata['page-title'] || x.metadata.heading ||
-      path.basename(rootie).replace(/[-_]/g, ' ');
+    //set these: pageTitle, pageTitleShort, windowTitle, bodyCopy
 
-    if (!x.heading) x.heading = x.metadata.heading || x.pageTitle;
+    if (x.metadata['page-title']) {
+      x.bodyCopy = x.html;
+      x.pageTitle = x.metadata['page-title'];
+    } else {
+      x.bodyCopy = x.html_no_heading;
+      x.pageTitle = x.heading || path.basename(rootie).replace(/[-_]/g, ' ');
+    }
 
+    x.windowTitle = x.metadata['window-title'] || x.pageTitle;
     x.pageTitleShort = x.metadata['page-title-short'] || x.pageTitle;
+
   });
 
-  datas.sort(function(a, b) {
-    return a.directoryIndex - b.directoryIndex;
-  });
+  datas.sort(function(a, b) { return a.directoryIndex - b.directoryIndex; });
 
   if (datas.length) {
     datas[0].isFirst = true;
